f3de0d9f89c896727a6322fb2b23ab47b6ce99e6
Dominic Galiano <dgaliano@utexas.edu>
2023-09-25 11:29:59 -0400

    string formatting

--- dbCRUD.js
+++ dbCRUD.js
@@ -203,7 +203,8 @@ class MyCLIApp extends CLIApp {
     console.log(
       `--new to add a new todo item
 --list [all|pending|done] to list the todo items
---done [id] to update a todo item--delete [id] to delete a todo item
+--done [id] to update a todo item
+--delete [id] to delete a todo item
 --help to list all the available options
 --version to print the version of the application`
     );
